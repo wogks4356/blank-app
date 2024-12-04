ac7ce64842506047a69cad6329e6ffb70f3106ee
wogks4356 <117740630+wogks4356@users.noreply.github.com>
2024-12-04 20:28:05 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,5 @@ import streamlit as st
 st.title("🎈 My 나의 app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "우리 같이 즐겨보아요"
 )
