a7c93f79020699a034e95d2040a74656a9fdc2ea
wogks4356 <117740630+wogks4356@users.noreply.github.com>
2024-12-19 16:56:21 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -439,6 +439,14 @@ elif st.session_state.page == "업데이트":
     st.image("https://i.pinimg.com/originals/61/4b/b4/614bb4d7982f87558039fc55fb223ef5.gif",  use_container_width=True)
     st.image("https://mblogthumb-phinf.pstatic.net/MjAyMDA3MDJfMjk4/MDAxNTkzNjc1MzM5NjIx.OjEij9RK6k3yFrvDhkRC0_3NXmfFqZiHUS1tyv-Fygwg.Wk6unZQiMuqoJeqfrDhIhUNIpiuj3tumQI_WyP7a2Wog.GIF.sjlhome/Despicable_Me_2_2013_1080p_BRRip_x264_AC3-JYK.mkv_001209458.gif?type=w800", use_container_width=True)
     st.write("Coming soon~")
+    st.markdown(
+        """
+        <div style="position: fixed; bottom: 10px; right: 10px;">
+            <img src="https://your-small-image-url-here.com/small-image.gif" style="width: 100px; height: auto;">
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
 #     st.markdown(
 #     """
