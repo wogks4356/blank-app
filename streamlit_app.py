b92589536d96f6d20720d76c84127529603a332e
wogks4356 <117740630+wogks4356@users.noreply.github.com>
2024-12-19 18:24:07 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy
 import base64
-
+from scipy.signal import savgol_filter, find_peaks
 
 import streamlit as st
 import pandas as pd
